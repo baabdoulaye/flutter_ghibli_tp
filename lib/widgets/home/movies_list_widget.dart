59756d2de6ed0c87a0c421f98eb117d6e2599044
laukho <lkhouri92@gmail.com>
2025-07-18 10:03:01 +0200

    movies

--- lib/widgets/home/movies_list_widget.dart
+++ lib/widgets/home/movies_list_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:ghibli/models/movie.dart';
 import 'package:ghibli/services/movies_api_service.dart';
 
 class MoviesListWidget extends StatelessWidget {
@@ -12,11 +13,46 @@ class MoviesListWidget extends StatelessWidget {
         print : déboguer des données simples
         inspect : déboguer des données complexes (Map et List)
     */
-    inspect(MoviesApiService().getMovies());
+    // inspect(MoviesApiService().getMovies());
 
     return Column(
       children: [
         Text('Movies', style: Theme.of(context).textTheme.titleMedium),
+        /*
+			pour accéder aux données d'une future, il est obligatoire d'utiliser le widget FutureBuilder
+		*/
+        FutureBuilder(
+          future: MoviesApiService().getMovies(),
+          builder: (context, snapshot) {
+            // snapshot contient les données de la future
+            // si les données sont disponibles
+            if (snapshot.hasData) {
+              // récupérer les données de la future
+              List<Movie> movies = snapshot.requireData;
+
+              // afficher une liste défilante
+              return ListView.builder(
+                // la hauteur de la liste s'adapte à son contenu
+                shrinkWrap: true,
+
+                // nombre d'éléments de la liste à créer
+                itemCount: movies.length,
+
+                // créer chaque élément de la liste
+                itemBuilder: (context, index) {
+                  /*
+						? : exécuter la partie de droite si la partie de gauche n'est pas nulle
+						! : indiquer que la valeur n'est pas nulle
+					*/
+                  return Text(movies[index].title!);
+                },
+              );
+            }
+
+            // si les données ne sont pas disponibles
+            return CircularProgressIndicator();
+          },
+        ),
       ],
     );
   }
